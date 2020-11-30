bb9ea45d75f8b25fb94f821eade62080f1a234f4
Diboune Saadi <saadi.diboune@etu.umontpellier.fr>
2020-11-30 18:45:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eapplication-back
---
+++ src/main/java/com/eapplication/eapplicationback/EapplicationBackApplication.java
@@ -0,0 +1,13 @@
+package com.eapplication.eapplicationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapplicationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EapplicationBackApplication.class, args);
+    }
+
+}
